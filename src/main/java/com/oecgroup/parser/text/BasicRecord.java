af36881e71de137a797cf6159b695db909d11012
jimmy923 <jimmy00142707@gmail.com>
2019-04-02 14:21:00 +0800

    解析字段

--- src/main/java/com/oecgroup/parser/text/BasicRecord.java
+++ src/main/java/com/oecgroup/parser/text/BasicRecord.java
@@ -12,36 +12,197 @@ import java.util.Date;
 @Record
 public class BasicRecord {
 
-  private String stringData;
-  private Integer integerData;
-  private Date dateData;
+  private String MblNo;
+  private String EquipmentInitial;
+  private String EquipmentNumber;
+  private String EquipmentCheckDigit;
+  private String EventTypeCode;
+  private Date EventDate;
+  private String CurrentLocationUNLCODE;
+  private String SightingCity;
+  private String SightingState;
+  private String SCAC;
+  private String ETACentury;
+  private Date RetaEventDate;
+  private String DestinationLocationUNLCODE;
+  private String DestinationCity;
+  private String DestinationState;
+  private String ReportingRailroadSCAC;
 
-
-  @Field(offset = 1, length = 10)
-  public String getStringData() {
-    return stringData;
+  // 2
+  @Field(offset = 2, length = 4)
+  public String getEquipmentInitial() {
+    return EquipmentInitial;
   }
 
-  public void setStringData(String stringData) {
-    this.stringData = stringData;
+  public void setEquipmentInitial(String EquipmentInitial) {
+    this.EquipmentInitial = EquipmentInitial;
   }
 
-  @Field(offset = 11, length = 5, align = Align.RIGHT, paddingChar = '0')
-  public Integer getIntegerData() {
-    return integerData;
+  // 3
+  @Field(offset = 6, length = 6)
+  public String getEquipmentNumber() {
+    return EquipmentNumber;
   }
 
-  public void setIntegerData(Integer integerData) {
-    this.integerData = integerData;
+  public void setEquipmentNumber(String EquipmentNumber) {
+    this.EquipmentNumber = EquipmentNumber;
   }
 
-  @Field(offset = 16, length = 10)
-  @FixedFormatPattern("yyyy-MM-dd")
-  public Date getDateData() {
-    return dateData;
+  // 4 ~ 9
+  @Field(offset = 12, length = 12)
+  @FixedFormatPattern("yyyyMMddHHmm")
+  public Date getEventDate() {
+    return EventDate;
   }
 
-  public void setDateData(Date dateData) {
-    this.dateData = dateData;
+  public void setEventDate(Date EventDate) {
+    this.EventDate = EventDate;
+  }
+
+  // 12
+  @Field(offset = 27, length = 9, align = Align.LEFT)
+  public String getSightingCity() {
+    return SightingCity;
+  }
+
+  public void setSightingCity(String SightingCity) {
+    this.SightingCity = SightingCity;
+  }
+
+  // 13
+  @Field(offset = 36, length = 2)
+  public String getSightingState() {
+    return SightingState;
+  }
+
+  public void setSightingState(String SightingState) {
+    this.SightingState = SightingState;
+  }
+
+  // 23
+  @Field(offset = 68, length = 4)
+  public String getReportingRailroadSCAC() {
+    return ReportingRailroadSCAC;
+  }
+
+  public void setReportingRailroadSCAC(String ReportingRailroadSCAC) {
+    this.ReportingRailroadSCAC = ReportingRailroadSCAC;
+  }
+
+  // 26
+  @Field(offset = 74, length = 9)
+  public String getDestinationCity() {
+    return DestinationCity;
+  }
+
+  public void setDestinationCity(String DestinationCity) {
+    this.DestinationCity = DestinationCity;
+  }
+
+  // 27
+  @Field(offset = 83, length = 2)
+  public String getDestinationState() {
+    return DestinationState;
+  }
+
+  public void setDestinationState(String DestinationState) {
+    this.DestinationState = DestinationState;
+  }
+
+  // 34
+  @Field(offset = 117, length = 2)
+  public String getETACentury() {
+    return ETACentury;
+  }
+
+  public void setETACentury(String ETACentury) {
+    this.ETACentury = ETACentury;
+  }
+
+  // 34 ~ 38
+  @Field(offset = 117, length = 10)
+  @FixedFormatPattern("yyyyMMddHH")
+  public Date getRetaEventDate() {
+    return RetaEventDate;
+  }
+
+  public void setRetaEventDate(Date RetaEventDate) {
+    this.RetaEventDate = RetaEventDate;
+  }
+
+  // 39
+  @Field(offset = 127, length = 4)
+  public String getEventTypeCode() {
+    return EventTypeCode;
+  }
+
+  public void setEventTypeCode(String EventTypeCode) {
+    this.EventTypeCode = EventTypeCode;
+  }
+
+  // 41
+  @Field(offset = 132, length = 5)
+  public String getCurrentLocationUNLCODE() {
+    return CurrentLocationUNLCODE;
+  }
+
+  public void setCurrentLocationUNLCODE(String CurrentLocationUNLCODE) {
+    this.CurrentLocationUNLCODE = CurrentLocationUNLCODE;
+  }
+
+  // 43
+  @Field(offset = 138, length = 5)
+  public String getDestinationLocationUNLCODE() {
+    return DestinationLocationUNLCODE;
+  }
+
+  public void setDestinationLocationUNLCODE(String DestinationLocationUNLCODE) {
+    this.DestinationLocationUNLCODE = DestinationLocationUNLCODE;
+  }
+
+  // 45
+  @Field(offset = 144, length = 1)
+  public String getEquipmentCheckDigit() {
+    return EquipmentCheckDigit;
+  }
+
+  public void setEquipmentCheckDigit(String EquipmentCheckDigit) {
+    this.EquipmentCheckDigit = EquipmentCheckDigit;
+  }
+
+  // 47
+  @Field(offset = 146, length = 20, align = Align.LEFT)
+  public String getMblNo() {
+    return MblNo;
+  }
+
+  public void setMblNo(String MblNo) {
+    this.MblNo = MblNo;
+  }
+
+  // 49
+  @Field(offset = 167, length = 4)
+  public String getSCAC() {
+    return SCAC;
+  }
+
+  public void setSCAC(String SCAC) {
+    this.SCAC = SCAC;
+  }
+
+  // 2 + 3 +45
+  public String getCntrNo() {
+    return getEquipmentInitial() + getEquipmentNumber() + getEquipmentCheckDigit();
+  }
+
+  // 12 + 13
+  public String getEventLocName() {
+    return getSightingCity() + "," + getSightingState();
+  }
+
+  // 26 + 27
+  public String getRetaEventLocName() {
+    return getDestinationCity() + "," + getDestinationState();
   }
 }
--- src/main/java/com/oecgroup/parser/text/BasicUsage.java
+++ src/main/java/com/oecgroup/parser/text/BasicUsage.java
@@ -3,22 +3,75 @@ package com.oecgroup.parser.text;
 import com.ancientprogramming.fixedformat4j.format.FixedFormatManager;
 import com.ancientprogramming.fixedformat4j.format.impl.FixedFormatManagerImpl;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by Allis Kuo on 2019-03-19
  */
 public class BasicUsage {
 
   private static FixedFormatManager manager = new FixedFormatManagerImpl();
+  private static String txtPath = "D://Project/Java/ediConn/tmp/OECGROUP_CLM.E9F601C1.20190329023216313";
 
   public static void main(String[] args) {
-    String string = "string    001232008-05-29";
-    BasicRecord record = manager.load(BasicRecord.class, string);
 
-    System.out.println("The parsed string: " + record.getStringData());
-    System.out.println("The parsed integer: " + record.getIntegerData());
-    System.out.println("The parsed date: " + record.getDateData());
+    File txtFile = new File(txtPath);
+    if(txtFile.exists()){
+      BufferedReader reader = null;
+      try {
+        reader = new BufferedReader(new FileReader(txtFile));
+        String tempString = null;
 
-    record.setIntegerData(100);
-    System.out.println("Exported: " + manager.export(record));
+        while ((tempString = reader.readLine()) != null) {
+          if(tempString.length() >= 170){
+            BasicRecord record = manager.load(BasicRecord.class, tempString);
+
+            System.out.println("---------------------------------------------------------------------");
+            System.out.println("The MBL NO: " + record.getMblNo());
+            System.out.println("The CNTR NO: " + record.getCntrNo());
+            System.out.println("The Event Type Code: " + record.getEventTypeCode());
+            System.out.println("The Event Date: " + record.getEventDate());
+            System.out.println("The Event Log: " + record.getCurrentLocationUNLCODE());
+            System.out.println("The Event Loc Name: " + record.getEventLocName());
+            System.out.println("The SCAC: " + record.getSCAC());
+            System.out.println("The File Name: " + txtFile.getName());
+            System.out.println("The Prtnr Event CD: " + record.getEventTypeCode());
+            System.out.println("The Reta Event Date: " + record.getRetaEventDate());
+            System.out.println("The Reta Event Log: " + record.getDestinationLocationUNLCODE());
+            System.out.println("The Reta Event Loc Name: " + record.getRetaEventLocName());
+            System.out.println("The RailRoad SCAC: " + record.getReportingRailroadSCAC());
+            System.out.println("Exported: " + manager.export(record));
+            System.out.println("---------------------------------------------------------------------");
+          }
+        }
+        reader.close();
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      }
+      finally {
+        if (reader != null) {
+          try {
+            reader.close();
+          } catch (IOException e1) {
+          }
+        }
+      }
+//      String string = "string    001232008-05-29";
+//      BasicRecord record = manager.load(BasicRecord.class, string);
+//
+//      System.out.println("The parsed string: " + record.getStringData());
+//      System.out.println("The parsed integer: " + record.getIntegerData());
+//      System.out.println("The parsed date: " + record.getDateData());
+//
+//      record.setIntegerData(100);
+//      System.out.println("Exported: " + manager.export(record));
+    }
+    else{
+      System.out.println("File Not Find");
+    }
   }
 }
